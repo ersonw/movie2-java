1803e1f212e3c0ddbccccbb7511a469a419814af
ersonw <asoonnode@gmail.com>
2022-05-18 20:00:46 +0800

    202205182000

---
+++ src/main/java/com/telebott/movie2java/control/WatchControl.java
@@ -0,0 +1,18 @@
+package com.telebott.movie2java.control;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "/api/watch", tags = "看片")
+@ApiResponses({
+        @ApiResponse(code = 200, message = "成功"),
+        @ApiResponse(code = 105, message = "未带token请求"),
+        @ApiResponse(code = 106, message = "token非法或者登录已过期"),
+})
+@RestController
+@RequestMapping("/api/watch")
+public class WatchControl {
+}
---
+++ src/main/java/com/telebott/movie2java/service/WatchService.java
@@ -0,0 +1,7 @@
+package com.telebott.movie2java.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WatchService {
+}
