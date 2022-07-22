ddad6f911f6869fab83e85031c5ef447ca49ef2b
ersonw <asoonnode@gmail.com>
2022-07-23 05:54:16 +0800

    202207230511

--- src/main/java/com/telebott/movie2java/dao/ShortVideoCommentDao.java
+++ src/main/java/com/telebott/movie2java/dao/ShortVideoCommentDao.java
@@ -24,6 +24,13 @@ public interface ShortVideoCommentDao extends JpaRepository<ShortVideoComment, L
     @Query(value = "SELECT svc.*,(SELECT COUNT(*) FROM short_video_comment_like WHERE comment_id = svc.id) AS c FROM `short_video_comment` AS svc WHERE  svc.reply_id =0 and svc.status=1 ORDER BY c,svc.pin DESC",nativeQuery = true)
     Page<ShortVideoComment> getAllComments(Pageable pageable);
 //    @Modifying
-    @Query(value = "SELECT svc.*,(SELECT COUNT(*) FROM `short_video_comment_like` AS svcl WHERE svcl.comment_id = svc.id) AS c FROM  `short_video_comment` AS svc LEFT JOIN `short_video_comment` AS svc2 ON svc2.reply_id= svc.id WHERE svc.reply_id =1 and svc.status=1 ORDER BY c,svc.pin DESC",nativeQuery = true)
+    @Query(value = "SELECT s.*,\n" +
+            "(SELECT COUNT(*) FROM `short_video_comment_like` AS svcl WHERE svcl.comment_id = s.id) AS c \n" +
+            "FROM \n" +
+            "(SELECT svc.* \n" +
+            " FROM `short_video_comment` AS svc \n" +
+            " INNER JOIN `short_video_comment` AS svc1 ON svc1.reply_id =1 \n" +
+            " INNER JOIN `short_video_comment` AS svc2 ON svc2.reply_id= svc1.id WHERE  svc.status=1) AS s \n" +
+            " ORDER BY c,s.pin DESC",nativeQuery = true)
     Page<ShortVideoComment> getAllByReplyId(long replyId,Pageable pageable);
 }
