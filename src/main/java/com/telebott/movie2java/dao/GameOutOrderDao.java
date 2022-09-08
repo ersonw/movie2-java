d7442b81c06b8449d04cb9751c5a718d8792a238
ersonw <asoonnode@gmail.com>
2022-09-09 02:51:21 +0800

    202209090236

--- src/main/java/com/telebott/movie2java/dao/GameOutOrderDao.java
+++ src/main/java/com/telebott/movie2java/dao/GameOutOrderDao.java
@@ -10,10 +10,10 @@ import org.springframework.data.repository.CrudRepository;
 public interface GameOutOrderDao extends JpaRepository<GameOutOrder, Long>, CrudRepository<GameOutOrder, Long> {
     GameOutOrder findAllById(long id);
     Page<GameOutOrder> findAllByUserId(long userId, Pageable pageable);
-    @Query(value = "SELECT IFNULL( SUM(amount), 0 )  FROM `game_out_order` WHERE user_id=:userId",nativeQuery = true)
+    @Query(value = "SELECT IFNULL( SUM(amount), 0 )  FROM `game_out_order` WHERE user_id=:userId AND status >= 0",nativeQuery = true)
     Long getAllBywBalance(long userId);
     @Query(value = "SELECT IFNULL( SUM(amount), 0 )  FROM `game_out_order` WHERE user_id=:userId AND status=:status",nativeQuery = true)
     Long getAllBywBalance(long userId, int status);
-    @Query(value = "SELECT IFNULL( SUM(amount), 0 )  FROM `game_out_order` WHERE user_id=:userId AND status <> 1",nativeQuery = true)
+    @Query(value = "SELECT IFNULL( SUM(amount), 0 )  FROM `game_out_order` WHERE user_id=:userId AND status =0",nativeQuery = true)
     Long getAllByFreezeBalance(long userId);
 }
